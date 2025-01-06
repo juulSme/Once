6e6971ac41152e4ffdeac0093f8b72f4baebe062
J. Smeets <16883019+juulSme@users.noreply.github.com>
2025-01-06 15:53:15 +0100

    mix new no_noncense

---
+++ lib/no_noncense_id.ex
@@ -0,0 +1,18 @@
+defmodule NoNoncenseId do
+  @moduledoc """
+  Documentation for `NoNoncenseId`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NoNoncenseId.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/no_noncense_id_test.exs
@@ -0,0 +1,8 @@
+defmodule NoNoncenseIdTest do
+  use ExUnit.Case
+  doctest NoNoncenseId
+
+  test "greets the world" do
+    assert NoNoncenseId.hello() == :world
+  end
+end
